dcf9b5502dc7edfc952297e335aa8efd64337258
Stephen Wan <Stephen@stephenwan.net>
2013-01-24 16:21:39 -0600

    added model for food nutrition

---
+++ src/com/stephenwan/libutfood/model/FoodNutritionFacts.java
@@ -0,0 +1,39 @@
+package com.stephenwan.libutfood.model;
+
+import java.util.HashMap;
+
+public class FoodNutritionFacts {
+	public FoodNutritionFacts(String name, float servingSize, String servingUnit, int calories, int caloriesFromFat, HashMap<String, FoodNutritionItem> items)
+	{
+		this.name = name;
+		this.servingSize = servingSize;
+		this.servingUnit = this.servingUnit;
+		this.calories = calories;
+		this.calories = caloriesFromFat;
+		this.items = items;
+	}
+	
+	private String name;
+	private float servingSize;
+	private String servingUnit;
+	private int calories;
+	private int caloriesFromFat;
+	private HashMap<String, FoodNutritionItem> items;
+	
+	public static class FoodNutritionItem
+	{
+		public FoodNutritionItem(String name, float amount, String amountUnit, float dVPercentage)
+		{
+			this.name = name;
+			this.amount = amount;
+			this.amountUnit = amountUnit;
+			this.dVPercentage = dVPercentage;
+			
+		}
+		
+		private String name;
+		private float amount;
+		private String amountUnit;
+		private float dVPercentage;
+	}
+}
